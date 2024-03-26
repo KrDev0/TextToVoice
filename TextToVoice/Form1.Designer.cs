0606e49cc083ad2d83fcce5159b2af6e142da71c
KrDev0 <christianarcila5@gmail.com>
2024-03-25 23:00:29 -0500

    Agregar archivos de proyecto.

---
+++ TextToVoice/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace TextToVoice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboSelectVoice = new System.Windows.Forms.ComboBox();
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cboSelectVoice
+            // 
+            this.cboSelectVoice.FormattingEnabled = true;
+            this.cboSelectVoice.Items.AddRange(new object[] {
+            "No establecido",
+            "Masculino",
+            "Femenina",
+            "Neutral"});
+            this.cboSelectVoice.Location = new System.Drawing.Point(155, 19);
+            this.cboSelectVoice.Name = "cboSelectVoice";
+            this.cboSelectVoice.Size = new System.Drawing.Size(98, 21);
+            this.cboSelectVoice.TabIndex = 0;
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Location = new System.Drawing.Point(259, 17);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(75, 23);
+            this.btnSpeak.TabIndex = 1;
+            this.btnSpeak.Text = "Iniciar";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Seleccionar genero de voz:";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(15, 46);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(481, 211);
+            this.txtContent.TabIndex = 3;
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(340, 17);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 1;
+            this.btnPause.Text = "Pausar";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Location = new System.Drawing.Point(421, 17);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(75, 23);
+            this.btnResume.TabIndex = 1;
+            this.btnResume.Text = "Reanudar";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 269);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnSpeak);
+            this.Controls.Add(this.cboSelectVoice);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TextToVoice";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboSelectVoice;
+        private System.Windows.Forms.Button btnSpeak;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnResume;
+    }
+}
+
---
+++ TextToVoice/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace TextToVoice
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer voice;
+        public Form1()
+        {
+            InitializeComponent();
+            voice = new SpeechSynthesizer();
+        }
+
+        private void btnSpeak_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                switch(cboSelectVoice.SelectedIndex)
+                {
+                    case 0:
+                        voice.SelectVoiceByHints(VoiceGender.NotSet);
+                        break;
+                    case 1:
+                        voice.SelectVoiceByHints(VoiceGender.Male);
+                        break;
+                    case 2:
+                        voice.SelectVoiceByHints(VoiceGender.Female);
+                        break;
+                    case 3:
+                        voice.SelectVoiceByHints(VoiceGender.Neutral);
+                        break;
+                    default:
+                        break;
+                }
+                voice.SpeakAsync(txtContent.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                voice.Pause();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                voice.Resume();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
